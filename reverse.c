45b5715e9e18ee08f97ff0778d04f79589a0b44e
ASHISH KUMAR JHA <ajha9108@gmail.com>
2025-10-07 22:47:37 +0530

    Create reverse.c

---
+++ reverse.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+    int arr[5], n;
+    printf("Enter n: ");
+    scanf("%d", &n);
+    for (int i=0; i<n; i++)
+        scanf("%d", &arr[i]);
+    for (int i=n-1; i>=0; i--)
+        printf("%d ", arr[i]);
+    return 0;
+}
